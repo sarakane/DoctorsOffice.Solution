e0790a904c972b384dae3e624a3ead901c62063f
Taylor Somers <tsomers@yandex.com>
2020-08-05 13:29:56 -0400

    Add Doctor class
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>

---
+++ DoctorsOffice/Models/Doctor.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace DoctorsOffice.Models
+{
+  public class Doctor
+  {
+    public Doctor()
+    {
+      this.Patients = new HashSet<DoctorPatient>();
+    }
+    public int DoctorId { get; set; }
+    public string Name { get; set; }
+    public string Specialty { get; set; }
+    public virtual ICollection<DoctorPatient> Patients { get; }
+  }
+}
