69fd5884a0fa8f6a4001686366b9514232f3eaa0
Taylor Somers <tsomers@yandex.com>
2020-08-05 13:46:29 -0400

    Add boilerplate and change namespace and other references
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>
    Co-authored-by: Joseph Pearce <joseph.pearce05@gmail.com>

---
+++ DoctorsOffice/Models/DesignTimeDbContextFactory.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DoctorsOffice.Models
+{
+  public class DoctorsOfficeContextFactory : IDesignTimeDbContextFactory<DoctorsOfficeContext>
+  {
+
+    DoctorsOfficeContext IDesignTimeDbContextFactory<DoctorsOfficeContext>.CreateDbContext(string[] args)
+    {
+      IConfigurationRoot configuration = new ConfigurationBuilder()
+          .SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json")
+          .Build();
+
+      var builder = new DbContextOptionsBuilder<DoctorsOfficeContext>();
+      var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+      builder.UseMySql(connectionString);
+
+      return new DoctorsOfficeContext(builder.Options);
+    }
+  }
+}
