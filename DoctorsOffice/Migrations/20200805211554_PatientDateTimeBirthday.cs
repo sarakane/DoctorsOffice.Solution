32802d4a32818f37137926a6486676c884b6ad08
Taylor Somers <tsomers@yandex.com>
2020-08-05 17:53:16 -0400

    Change Patient Birthday data type to DateTime
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>
    Co-authored-by: Joseph Pearce <joseph.pearce05@gmail.com>

---
+++ DoctorsOffice/Migrations/20200805211554_PatientDateTimeBirthday.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DoctorsOffice.Migrations
+{
+    public partial class PatientDateTimeBirthday : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthday",
+                table: "Patients",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Birthday",
+                table: "Patients",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+        }
+    }
+}
