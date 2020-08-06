37f2816d4a0b72d4d2abf31f548dd00bfb6df7ba
Taylor Somers <tsomers@yandex.com>
2020-08-06 13:32:56 -0400

    Add functionality to correctly link to Delete view and delete patient from database
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>
    Co-authored-by: Joseph Pearce <joseph.pearce05@gmail.com>

--- DoctorsOffice/Controllers/PatientsController.cs
+++ DoctorsOffice/Controllers/PatientsController.cs
@@ -79,5 +79,29 @@ namespace DoctorsOffice.Controllers
       _db.SaveChanges();
       return RedirectToAction("Details", new { id = patient.PatientId});
     }
+
+    public ActionResult Delete(int id)
+    {
+      var thisPatient = _db.Patients.FirstOrDefault(patients => patients.PatientId == id);
+      return View(thisPatient);
+    }
+
+    [HttpPost]
+    public ActionResult RemoveDoctor(int joinId)
+    {
+      var joinEntry = _db.DoctorPatient.FirstOrDefault(entry => entry.DoctorPatientId == joinId);
+      _db.DoctorPatient.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = joinEntry.PatientId });
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisPatient = _db.Patients.FirstOrDefault(patients => patients.PatientId == id);
+      _db.Patients.Remove(thisPatient);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
