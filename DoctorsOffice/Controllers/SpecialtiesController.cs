2daa66d0e52d9128af9b949ecca869254d6c2ff5
Taylor Somers <tsomers@yandex.com>
2020-08-06 17:56:25 -0400

    Add HTTPPost action to associate a doctor with the selected specialty
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>
    Co-authored-by: Joseph Pearce <joseph.pearce05@gmail.com>

--- DoctorsOffice/Controllers/SpecialtiesController.cs
+++ DoctorsOffice/Controllers/SpecialtiesController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DoctorsOffice.Controllers
 {
@@ -35,6 +36,33 @@ namespace DoctorsOffice.Controllers
       }
 
       return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisSpecialty = _db.Specialties
+      .Include(specialty => specialty.Doctors)
+      .ThenInclude(join => join.Doctor)
+      .FirstOrDefault(specialty => specialty.SpecialtyId == id);
+      return View(thisSpecialty);
+    }
+
+    public ActionResult AddDoctor(int id)
+    {
+      var thisSpecialty = _db.Specialties.FirstOrDefault(specialties => specialties.SpecialtyId == id);
+      ViewBag.DoctorId = new SelectList(_db.Doctors, "DoctorId", "Name");
+      return View(thisSpecialty);
+    } 
+
+    [HttpPost]
+    public ActionResult AddDoctor(Specialty specialty, int DoctorId)
+    {
+      if(DoctorId != 0 && !_db.DoctorSpecialty.Any(x => x.DoctorId == DoctorId && x.SpecialtyId == specialty.SpecialtyId))
+      {
+        _db.DoctorSpecialty.Add(new DoctorSpecialty() { DoctorId = DoctorId, SpecialtyId = specialty.SpecialtyId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = specialty.SpecialtyId});
     }
   }
 }
